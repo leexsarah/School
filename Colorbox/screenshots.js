477cdd4d259decdebc6624feac5e8e15bc8e49c1
Sarah Lee <sarahyoungahlee91@gmail.com>
2015-03-18 15:44:38 -0700

    Colorbox Javascript program

---
+++ Colorbox/screenshots.js
@@ -0,0 +1,13 @@
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+ 
+// Server-side code
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+
+var main=function(){
+	"use strict";
+	$("a.screenshots").colorbox({opacity:0.5 , rel:"a.screenshots", width:"90%", 
+								height:"90%", transition:"fade", slideshow:true });
+}
+
+$(document).ready(main);
